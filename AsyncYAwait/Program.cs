2b029d6ec22898cbb0c7fcdbc416f52a1999290b
Yordy Nuñez <yordierik05@gmail.com>
2024-08-09 17:40:29 -0500

    Se hizo la practica de la evaluación de C#

---
+++ AsyncYAwait/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Program
+{
+    public static async Task Main(string[] args) 
+    {
+        Console.WriteLine("Iniciando tarea...");
+
+        String resultado = await ObtenerDatos();
+
+        Console.WriteLine("Tarea Completa");
+        Console.WriteLine($"Resultado: {resultado}");
+    }
+
+    public static async Task<String> ObtenerDatos() 
+    {
+        Console.WriteLine("Obteniendo datos....");
+        await Task.Delay(3000);
+
+        return "Datos obtenidos exitosamente";
+    }
+}
---
+++ Practica/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+public class Persona
+{
+    public String Nombre { get; set; }
+    public int Edad { get; set; }
+    public String Direccion { get; set; }
+
+    public Persona(String nombre, int edad, String direccion)
+    {
+        Nombre = nombre;
+        Edad = edad;
+        Direccion = direccion;
+    }
+
+    public Persona()
+    {
+        Nombre = "Yordy";
+        Edad = 19;
+        Direccion = "Neiva";
+    }
+
+    // Método virtual que puede ser sobrescrito en la clase derivada
+    public virtual void MostrarInformacion()
+    {
+        Console.WriteLine($"Nombre: {Nombre}");
+        Console.WriteLine($"Edad: {Edad}");
+        Console.WriteLine($"Dirección: {Direccion}");
+    }
+
+    public static void Main(String[] args)
+    {
+        Empleado empleado = new Empleado();
+        empleado.MostrarInformacion();
+    }
+}
+
+public class Empleado : Persona
+{
+    public double Salario { get; set; }
+
+    // Constructor que inicializa los atributos heredados y el nuevo atributo
+    public Empleado(String nombre, int edad, String direccion, double salario)
+        : base(nombre, edad, direccion) 
+    {
+        Salario = salario;
+    }
+
+    public Empleado()
+    {
+        Nombre = "Yordy";
+        Edad = 19;
+        Direccion = "Neiva";
+        Salario = 1000;
+    }
+
+    // Sobrescribir el método para mostrar la información 
+    public override void MostrarInformacion()
+    {
+        base.MostrarInformacion();
+        Console.WriteLine($"Salario: {Salario}");
+    }
+}
